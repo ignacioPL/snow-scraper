1e88f843baf239ce26aa2c69c2aee400a7f4055b
Gisela Bellisomi <gisela.bellisomi@ar.infra.d>
2020-04-30 20:16:21 -0300

    Fix bug in download/reading when the url is wrong

--- src/main/scala/gb/io/snow/scraper/services/Downloader.scala
+++ src/main/scala/gb/io/snow/scraper/services/Downloader.scala
@@ -1,13 +1,22 @@
 package gb.io.snow.scraper.services
 import scalaj.http._
 
+import scala.util.{Failure, Success, Try}
+
 trait Downloader {
   def downloadLastData(url :String): Array[Byte]
 }
 
 case class DownloaderImpl() extends Downloader  {
   override def downloadLastData(url :String): Array[Byte] = {
-    val byte: Array[Byte] = Http(url).asBytes.body
-    byte
+    //val byte: Array[Byte] = Http(url).asBytes.body
+    val byteDocument: Try[HttpResponse[Array[Byte]]]= Try( Http(url).asBytes )
+
+    val bodyDocument: Array[Byte] = byteDocument match {
+      case Success(value) if value.code==200 => value.body
+      case _ => Array.emptyByteArray
+    }
+    bodyDocument
+    //byte
   }
 }
--- src/main/scala/gb/io/snow/scraper/services/PdfReader.scala
+++ src/main/scala/gb/io/snow/scraper/services/PdfReader.scala
@@ -4,6 +4,7 @@ import gb.io.snow.scraper.models.CovidData
 import java.io.File
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.text.PDFTextStripper
+import scala.util.{Failure, Success, Try}
 
 
 trait PdfReader {
@@ -14,19 +15,18 @@ trait PdfReader {
 case class PdfReaderImpl() extends PdfReader {
 
   override def readPdf(byte: Array[Byte]): CovidData= {
+    if (byte.nonEmpty ) {
 
-    //val file = new File(filePath)
-    val document: PDDocument = PDDocument.load(byte)
+      val document: PDDocument = PDDocument.load(byte)
+      val pdfStripper = new PDFTextStripper()
+      val text: String = pdfStripper.getText(document)
+      document.close()
 
-    val pdfStripper = new PDFTextStripper()
-    val text: String = pdfStripper.getText(document)
-    document.close()
-
-    val date: String = text.split("REPORTE DIARIO VESPERTINO NRO")(0).trim
-
-    val stringPostNumberOfCases: String = text.split("Hoy fueron confirmados ")(1)
-    val numberOfCases: Int = stringPostNumberOfCases.split(" nuevos casos de COVID-19")(0).toInt
-
-    CovidData(date,numberOfCases)
+      val date: String = text.split("REPORTE DIARIO VESPERTINO NRO")(0).trim
+      val stringPostNumberOfCases: String = text.split("Hoy fueron confirmados ")(1)
+      val numberOfCases: Int = stringPostNumberOfCases.split(" nuevos casos de COVID-19")(0).toInt
+      CovidData(date, numberOfCases)
+    } else {
+      CovidData("",0) }
   }
 }
