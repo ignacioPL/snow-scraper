fdd4c457096a82031d9fad7929de29566f5a914b
Gisela Bellisomi <gisela.bellisomi@ar.infra.d>
2020-05-01 18:32:24 -0300

    WriterImpl

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.12.11"
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.1.1" % Test,
   "org.apache.pdfbox" % "pdfbox" % "2.0.19",
-  "org.scalaj" %% "scalaj-http" % "2.4.2"
+  "org.scalaj" %% "scalaj-http" % "2.4.2",
+  "com.github.pathikrit" %% "better-files" % "3.8.0"
 )
--- src/main/scala/gb/io/snow/scraper/Run.scala
+++ src/main/scala/gb/io/snow/scraper/Run.scala
@@ -2,7 +2,7 @@ package gb.io.snow.scraper
 
 
 import gb.io.snow.scraper.models.CovidData
-import gb.io.snow.scraper.services.{DownloaderImpl, PdfReaderImpl}
+import gb.io.snow.scraper.services.{DownloaderImpl, PdfReaderImpl,WriterImpl}
 import org.apache.pdfbox.pdmodel.PDDocument
 
 object Run extends App{
@@ -14,8 +14,10 @@ object Run extends App{
 
   val pdfReaderImpl = PdfReaderImpl()
   val covidData: CovidData = pdfReaderImpl.readPdf(documentAsByte)
-
   println(covidData)
+
+  val w :WriterImpl = WriterImpl()
+  val ww =w.writeCsv("C:/Users/gisela.bellisomi/Documents/personal/corona/snow-scraper/casos_crorona_arggob.csv",covidData)
 
   println("Finish")
 }
